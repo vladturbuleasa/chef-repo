623d406c14e4dac4cf5444d26000b278cfc54b01
Vlad Turbuleasa-Jurje <vlad.turbuleasa@gmail.com>
2016-03-03 12:25:20 +0200

    Update default.rb

--- cookbooks/servlet-maven/recipes/default.rb
+++ cookbooks/servlet-maven/recipes/default.rb
@@ -9,8 +9,7 @@
 include_recipe "tomcat"
 include_recipe "yum-servlet"
 
-cookbook_file "/etc/yum.conf" do
-	source "yum.conf"
-	mode "0644"
+package 'ServletGradle' do
+        action :install
 end
 
